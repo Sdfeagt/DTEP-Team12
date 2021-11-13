c4361515c9327432ab9247275e6d0f893b2d8235
Rafal <rafalciechanski15@gmail.com>
2021-11-13 18:12:53 +0200

    First commit

---
+++ HC-SR04-for_all_3.ino
@@ -0,0 +1,108 @@
+// This code uses snippets from https://dronebotworkshop.com/hc-sr04-ultrasonic-distance-sensor-arduino/
+// Check out the page, has a lot of useful info when it comes to HC-SR04
+
+
+#include "DHT.h";
+#include "NewPing.h"
+ 
+ 
+#define DHTPIN 7       // DHT-22 Output Pin connection
+#define DHTTYPE DHT22   // DHT Type is DHT 22 (AM2302)
+#define TRIGGER_PIN_1  10
+#define ECHO_PIN_1     10
+#define TRIGGER_PIN_2  5
+#define ECHO_PIN_2     5
+#define MAX_DISTANCE 400
+ 
+NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
+NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
+NewPing sonar3(TRIGGER_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
+ 
+// Define Variables
+float duration1; // Stores First HC-SR04 pulse duration value
+float duration2; // Stores Second HC-SR04 pulse duration value
+float distance1; // Stores calculated distance in cm for First Sensor
+float distance2; // Stores calculated distance in cm for Second Sensor
+float distance3; // Stores calculated distance in cm for Third Sensor
+int iterations = 5;
+ 
+// Initialize DHT sensor for normal 16mhz Arduino
+ 
+DHT dht(DHTPIN, DHTTYPE); 
+ 
+void setup() {
+  Serial.begin (9600);
+  dht.begin();
+}
+ 
+void loop()
+{
+ 
+  delay(1000);  // Delay so DHT-22 sensor can stabalize
+   
+  
+  // Measure duration for first sensor
+    
+  duration1 = sonar1.ping_median(iterations);
+  
+  // Add a delay between sensor readings
+  
+  delay(1000);
+  
+  // Measure duration for first sensor
+  
+  duration2 = sonar2.ping_median(iterations);
+
+    delay(1000);
+  
+  // Measure duration for first sensor
+  
+  duration3 = sonar3.ping_median(iterations);
+  
+  // Calculate the distances for both sensors
+  
+  distance1 = (duration1 / 2) * 0.0343;
+  distance2 = (duration2 / 2) * 0.0343;
+  distance3 = (duration3 / 2) * 0.0343;
+  
+  // Send results to Serial Monitor
+  
+    Serial.print("Distance 1: ");
+ 
+    if (distance1 >= 400 || distance1 <= 2) {
+    Serial.print("Out of range");
+    }
+    else {
+    Serial.print(distance1);
+    Serial.print(" cm ");
+    if (distance1 <= 10){
+      //Make vibrator go brrrrr
+    }
+    }
+    
+    Serial.print("Distance 2: ");
+ 
+    if (distance2 >= 400 || distance2 <= 2) {
+    Serial.print("Out of range");
+    }
+    else {
+    Serial.print(distance2);
+    Serial.print(" cm");
+    if (distance1 <= 10){
+    //Make vibrator go brrrrr
+    }
+    }
+
+    if (distance3 >= 400 || distance3 <= 2) {
+    Serial.print("Out of range");
+    }
+    else {
+    Serial.print(distance2);
+    Serial.print(" cm");
+    if (distance1 <= 10){
+    //Make vibrator go brrrrr
+    }
+    }
+  
+  Serial.println(" ");
+}
